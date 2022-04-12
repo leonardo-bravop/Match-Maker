6b30a2d2db2019d5716b289c3e178fddcba5a531
agustin <agustinporta2000@gmail.com>
2022-04-12 12:20:12 -0300

    agus

--- api/routes/match.js
+++ api/routes/match.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const matchController = require("../controllers/matchController")
+const userController = require("../controllers/userController")
 
 router.post("/newMatch", matchController.newMatch);
 
-router.delete("/deleteMatch/:id", matchController.deleteMatch);
+router.delete("/deleteMatch/:id", userController.verifyToken ,matchController.deleteMatch);
 
 router.get("/showMatch/:id", matchController.findMatch);
 
-router.delete("/deleteAll", matchController.deleteAll)
+router.delete("/deleteAll", userController.verifyToken ,matchController.deleteAll)
 
 module.exports = router;
